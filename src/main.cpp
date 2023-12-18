dfa022ffb7afb066a37d2da642131dc5f20c023a
David Chemaly <davidchemaly1@gmail.com>
2023-12-18 23:11:27 +0100

    add imgui for application

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
-#include "get_info.h"
-#include "imageConverter.h"
+#include <iostream>
 
-int main() {
-    std::string filename = "/home/david/Desktop/Image/TVID/Projet/TVID_projet/test_images/pgm/bw_numbers/0.pgm";
-    ImageInfo info = readPGM(filename);
+#include "application.h"
 
-    std::cout << "Width: " << info.width << std::endl;
-    std::cout << "Height: " << info.height << std::endl;
-    std::cout << "Depth: " << info.depth << std::endl;
-    std::cout << "Sampling Mode: " << info.samplingMode << std::endl;
+// #include "get_info.h"
+// #include "imageConverter.h"
+// int main() {
+//     std::string filename = "/home/david/Desktop/Image/TVID/Projet/TVID_projet/test_images/pgm/bw_numbers/0.pgm";
+//     ImageInfo info = readPGM(filename);
 
-    std::string outputFilename = "/home/david/Desktop/Image/TVID/Projet/TVID_projet/test_images/ppm/bw_numbers/0.ppm";
-    ConvertPGMtoPPM(filename, outputFilename);
+//     std::cout << "Width: " << info.width << std::endl;
+//     std::cout << "Height: " << info.height << std::endl;
+//     std::cout << "Depth: " << info.depth << std::endl;
+//     std::cout << "Sampling Mode: " << info.samplingMode << std::endl;
+
+//     std::string outputFilename = "/home/david/Desktop/Image/TVID/Projet/TVID_projet/test_images/ppm/bw_numbers/0.ppm";
+//     ConvertPGMtoPPM(filename, outputFilename);
+
+//     return EXIT_SUCCESS;
+// }
+
+int main(int arg, char **argv)
+{
+    Application::Application app;
+
+    app.Run();
 
     return EXIT_SUCCESS;
 }
